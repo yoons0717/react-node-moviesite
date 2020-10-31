dcb1b215f61e776e41391c0e9021ae5f2d552c89
yoons0717 <yoons0717@gmail.com>
2020-10-31 18:01:08 +0900

    landingpage

---
+++ boilerplate-mern-stack-master/boilerplate-mern-stack-master/client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '7fb4d8c405af8d8c34ba0b9f937612cf'
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
---
+++ boilerplate-mern-stack-master/boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,67 @@
+import React, {useEffect,useState} from 'react'
+
+import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config';
+import MainImage from './Sections/MainImage'
+import GridCards from '../commons/GridCards';
+import {Row} from 'antd';
+
+function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            setMovies(response.results)
+            setMainMovieImage(response.results[0])
+           
+        })
+
+
+    }, [])
+
+    console.log(Movies)
+   
+    return (
+        <div style={{ width:'100%' , margin:'0'}}>
+           {/* main image */}
+           {MainMovieImage &&   // 이 값이 있으면 렌더링을 해라, 이거 안써주면, 이미지 정보를 가져오기 전에 페이지를 먼저 렌더링하여 오류 발생
+            <MainImage 
+            
+                image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                title={MainMovieImage.original_title}
+                text = {MainMovieImage.overview}
+                />
+            }           
+           <div style={{ width:'85%' , margin:'1rem auto'}}>
+                <h2>Movies by latest</h2>
+                <hr/>
+
+                {/* Movie Grid Cards */}
+                <Row gutter={[16,16]}>
+                {/* Movies가 있으면 */}
+                {Movies && Movies.map((movie,index)=>(
+                    <React.Fragment key={index}>
+
+                        <GridCards 
+                            image ={movie.poster_path ? 
+                                `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                            movieId = {movie.id}
+                            movieName={movie.original_title}
+                            
+                        />
+                    </React.Fragment>
+                ))}
+                </Row>
+           </div>
+           <div style={{ display:'flex' , justifyContent:'center'}}>
+                <button>Load More</button>
+           </div>
+        </div>
+    )
+}
+
+export default LandingPage
